91508cc8174f8e13e6b4f9ff837d7886a763cff0
Dave B <dave@bx.psu.edu>
2018-07-17 12:43:03 -0400

    Some tweaks and updates to the parser.

--- parse_gatk_json.py
+++ parse_gatk_json.py
@@ -4,14 +4,17 @@ from collections import OrderedDict
 from string import Template
 import argparse
 import json
+import pypandoc
 
 VERSION="0.1.0"
 
 def supply_args():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('json', help='Input JSON')
-    parser.add_argument('xml_out', help='Output XML')
+    parser.add_argument('--json', help='Input JSON')
+    parser.add_argument('--json_type', choices=['picard_vcf'], help='Pass the type of file we are working with, so format tag can be filled.')
+    parser.add_argument('--xml_out', help='Output XML')
     parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
+    parser.add_argument('--picard', dest='picard', action='store_true', default=False)
     args = parser.parse_args()
     return args
 
@@ -39,10 +42,12 @@ class JsonXml(object):
         """
         self.blob = blob
         self.type = blob['type']
+        self.section = blob['kind']
         self.xml_out = self.reblob()
         self.xml_write = self.template_xml()
         self.xml_param_out = []
         self.xml_param_out.append(self.xml_write)
+        # print(self.xml_out)
         for entry in self.template_sel():
             self.xml_param_out.append(entry)
         if self.xml_out['type'] == 'select':
@@ -54,12 +59,12 @@ class JsonXml(object):
         :return:
         """
 
-        templates = {'integer': Template('\t\t<param argument="$argument" type="$type" optional="$optional" value="$value" min="$min" max="$max" label="$label" help="$help" />\n'),
-                     'float': Template('\t\t<param argument="$argument" type="$type" optional="$optional" value="$value" min="$min" max="$max" label="$label" help="$help" />\n'),
-                     'text': Template('\t\t<param argument="$argument" type="$type" optional="$optional" value="$value" label="$label" help="$help" />\n'),
-                     'data': Template('\t\t<param argument="$argument" type="$type" optional="$optional" format="" label="$label" help="$help" />\n'),
-                     'select': Template('\t\t<param argument="$argument" type="$type" optional="$optional" label="$label" help="$help" >\n'),
-                     'boolean': Template('\t\t<param argument="$argument" truevalue="$truevalue" falsevalue="$falsevalue" type="$type" optional="$optional" checked="$checked" label="$label" help="$help" />\n')}
+        templates = {'integer': Template('\t\t<param name="$name" argument="$argument" type="$type" optional="$optional" value="$value" min="$min" max="$max" label="$label" help="$help" />\n'),
+                     'float': Template('\t\t<param name="$name" argument="$argument" type="$type" optional="$optional" value="$value" min="$min" max="$max" label="$label" help="$help" />\n'),
+                     'text': Template('\t\t<param name="$name" argument="$argument" type="$type" optional="$optional" value="$value" label="$label" help="$help" />\n'),
+                     'data': Template('\t\t<param name="$name" argument="$argument" type="$type" optional="$optional" format="" label="$label" help="$help" />\n'),
+                     'select': Template('\t\t<param name="$name" argument="$argument" type="$type" optional="$optional" label="$label" help="$help" >\n'),
+                     'boolean': Template('\t\t<param name="$name" argument="$argument" type="$type" truevalue="$truevalue" falsevalue="$falsevalue" optional="$optional" checked="$checked" label="$label" help="$help" />\n')}
         try:
             return templates[self.xml_out['type']].substitute(self.xml_out)
         except:
@@ -92,10 +97,10 @@ class JsonXml(object):
         <param name="max_records_in_ram" type="integer" size="10" value="500000" min="1" label="Max Records in RAM" help="When writing files that need to be sorted, this will specify the number of records stored in RAM before spilling to disk. Increasing this number reduces the number of file handles needed to sort the file, and increases the amount of RAM needed." />
         :return:
         """
-        xml_out = {'name': self.blob['name'].lstrip('-').lower(),
+        xml_out = {'name': self.blob['name'].lstrip('-').lower().replace('-', '_'),
                    'argument': self.blob['name'],
                    'type': self.xml_json_type_map(self.blob['type']),
-                   'label': self.blob['name'].lstrip('-').replace('_', ' ').title(),
+                   'label': self.blob['name'].lstrip('-').replace('-', ' ').title(),
                    'optional': self.xml_json_req_map(self.blob['required']),
                    'value': self.blob['defaultValue'],
                    'truevalue': self.blob['name'],
@@ -103,7 +108,8 @@ class JsonXml(object):
                    'checked': self.blob['defaultValue'],
                    'max': self.xml_json_num_map(self.blob['maxValue']),
                    'min': self.xml_json_num_map(self.blob['minValue']),
-                   'help': self.blob['summary']}
+                   'help': self.blob['summary'],
+                   'section': self.section}
         return xml_out
 
     def get_select_opts(self):
@@ -140,13 +146,19 @@ class JsonXml(object):
         xml_json_type_map = {
                              'Boolean': 'boolean',
                              'boolean': 'boolean',
+                             'Long': 'integer',
+                             'long': 'integer',
                              'Integer': 'integer',
                              'int': 'integer',
                              'byte': 'integer',
                              'Double': 'float',
                              'double': 'float',
+                             'Float': 'float',
+                             'float': 'float',
                              'String': 'text',
+                             'Set[String]': 'text',
                              'File': 'data',
+                             'Set[File]': 'data',
                              'List[File]': 'data',
                              'List[Double]': 'data',
                              'List[Integer]': 'data',
@@ -200,7 +212,14 @@ class JsonCheetah(JsonXml):
         Produce templates for how things looks in the Cheetah section.
         :return:
         """
-        cht_tmpl = Template('$argument $${$name}')
+        if self.xml_out['section'] not in ['required', 'common']:
+            template_string = '$argument $${$section}.{$name}'
+        else:
+            template_string = '$argument $${$name}'
+        if self.xml_out['type'] == 'boolean':
+            cht_tmpl = Template(template_string.replace('$argument ', ''))
+        else:
+            cht_tmpl = Template(template_string)
         return cht_tmpl.substitute(self.xml_out)
 
 
@@ -211,23 +230,66 @@ class JsonShell(object):
     description == help
     name == name
     """
-    def __init__(self, filename, profile='17.09'):
+    def __init__(self, filename, picard=True, profile='17.09'):
         """
 
         """
+        self.known_types = [ 'Boolean',
+                             'boolean',
+                             'Long',
+                             'long',
+                             'Integer',
+                             'int',
+                             'byte',
+                             'Double',
+                             'double',
+                             'Float',
+                             'float',
+                             'String',
+                             'Set[String]',
+                             'Set[File]',
+                             'File',
+                             'List[File]',
+                             'List[Double]',
+                             'List[Integer]',
+                             'List[String]',
+                             'FeatureInput[VariantContext]',
+                             'LogLevel',
+                             'Implementation',
+                             'IntervalMergingRule',
+                             'IntervalSetRule',
+                             'ValidationStringency',
+                             'GenotypingOutputMode',
+                             'OutputMode',
+                             'PCRErrorModel',
+                             'WriterType' ]
+
         self.common = ("--version", "--showHidden", "--help", "--arguments_file", "--VERBOSITY",
                        "--VALIDATION_STRINGENCY", "--USE_JDK_INFLATER", "--USE_JDK_DEFLATER", "--TMP_DIR", "--QUIET",
                        "--MAX_RECORDS_IN_RAM", "--GA4GH_CLIENT_SECRETS", "--CREATE_MD5_FILE", "--CREATE_INDEX",
                        "--COMPRESSION_LEVEL", "--REFERENCE_SEQUENCE", "--OUTPUT")
         self.profile = profile
+        self.picard = picard
         self.xml_params = []
         self.cheetah_params = []
+        self.sectional_params = {'optional': [], 'advanced': []}
         with open(filename, 'rU') as myfile:
             self.json_file = json.load(myfile)
             for entry in self.json_file['arguments']:
-                if entry['name'] not in self.common:
-                    self.xml_params.extend(JsonXml(entry).xml_param_out)
-                    self.cheetah_params.append(JsonCheetah(entry).cheetah_template())
+                if entry['type'] not in self.known_types:
+                    print('Argument type %s not recognized, skipping.' % entry['type'])
+                    continue
+                if entry['kind'] not in self.sectional_params:
+                    if entry['name'] not in self.common:
+                        self.xml_params.extend(JsonXml(entry).xml_param_out)
+                else:
+                    self.sectional_params[entry['kind']].extend([param.replace('\t\t', '\t\t\t') for param in JsonXml(entry).xml_param_out])
+                self.cheetah_params.append(JsonCheetah(entry).cheetah_template())
+            for section in self.sectional_params:
+                section_template = {'name': section, 'label': section.title()}
+                self.xml_params.append(Template('\t\t<section name="$name" label="$label Parameters" expanded="False">\n').substitute(section_template))
+                self.xml_params.extend(self.sectional_params[section])
+                self.xml_params.append('\t\t</section>\n')
 
     def get_shell(self, outfile):
         """
@@ -235,8 +297,11 @@ class JsonShell(object):
         :return:
         """
         self.handle_out = open(outfile, 'w')
+        shell_dict = self.build_shell_dict()
+        if 'cheetah_template' not in shell_dict:
+            shell_dict['cheetah_template'] = '\n\t\t'.join(self.cheetah_params)
         for k, v in self.build_shell_tmpl().items():
-            self.handle_out.write(v.substitute(self.build_shell_dict()))
+            self.handle_out.write(v.substitute(shell_dict).replace('\t', '    '))
             self.handle_out.write('\n')
             if k == 'inputs':
                 self.write_params()
@@ -248,19 +313,19 @@ class JsonShell(object):
         :return:
         """
         for entry in self.xml_params:
-            self.handle_out.write(entry)
+            self.handle_out.write(entry.replace('\t', '    '))
 
     def build_shell_dict(self):
         """
         This will house all values the templates need.
         :return:
         """
-        shell_dict = {'id': self.json_file['name'].lower().split(' ')[0],
+        shell_dict = {'id': self.json_file['name'],
                       'name': self.json_file['name'],
                       'short_name': self.json_file['name'].split(' ')[0],
                       'profile': self.profile,
                       'description': self.json_file['summary'].rstrip(' '),
-                      'summary': self.json_file['description']}
+                      'summary': pypandoc.convert_text(self.json_file['description'], 'rst', format='html')}
         return shell_dict
 
     def build_shell_tmpl(self):
@@ -268,18 +333,28 @@ class JsonShell(object):
         Provide templates for the shell of the XML file.
         :return:
         """
-        shell_tmpl = OrderedDict({'tool': Template('<tool id="gatk4_$id" name="$name" version="@VERSION@.0" profile="$profile">\n'),
-                                'description': Template('\t<description>- $description</description>\n'),
-                                'macros': Template('\t<macros>\n\t\t<import>macros.xml</import>\n\t\t<import>picard_macros.xml</import>\n\t</macros>\n'),
-                                'expand': Template('\t<expand macro="requirements"/>\n\t<expand macro="version_cmd"/>\n'),
-                                'command': Template('\t<command detect_errors="exit_code"><![CDATA[\n\t\t@CMD_BEGIN@ $short_name\n\t\t#include source=$$picard_ref_opts#\n\t\t#include source=$$picard_opts#\n\t\t#include source=$$picard_output_opts#\n\t]]></command>\n'),
-                                'inputs': Template('\t<inputs>\n\t\t<expand macro="gatk_req_params" />\n\t\t<expand macro="picard_params" />'),
-                                'inputs_close': Template('\t</inputs>\n'),
-                                'outputs': Template('\t<outputs>\n\t\t<expand macro="picard_output_params" />\n\t</outputs>\n'),
-                                'tests': Template('\t<tests>\n\t</tests>\n'),
-                                'help': Template('\t<help><![CDATA[\n\t$summary\n\t@PICARD_HELP@\n\t]]></help>\n'),
-                                'citations': Template('\t<expand macro="citations"/>\n'),
-                                'tool_close': Template('</tool>')})
+        shell_tmpl = OrderedDict([('tool', Template('<?xml version="1.0"?>\n<tool id="gatk4_$id" name="GATK4: $name" version="@WRAPPER_VERSION@0" profile="$profile">\n')),
+                                ('description', Template('\t<description>- $description</description>\n')),
+                                ('macros', ''),
+                                ('expand', Template('\t<expand macro="requirements"/>\n\t<expand macro="version_cmd"/>\n')),
+                                ('command', ''),
+                                ('inputs', ''),
+                                ('inputs_close', Template('\t</inputs>\n')),
+                                ('outputs', Template('\t<outputs>\n\t\t<expand macro="picard_output_params" />\n\t</outputs>\n')),
+                                ('tests', Template('\t<tests>\n\t</tests>\n')),
+                                ('help', Template('\t<help><![CDATA[\n\t$summary\n\t@PICARD_HELP@\n\t]]></help>\n')),
+                                ('citations', Template('\t<expand macro="citations"/>\n')),
+                                ('tool_close', Template('</tool>'))])
+
+        if self.picard:
+            shell_tmpl['macros'] = Template('\t<macros>\n\t\t<import>macros.xml</import>\n\t\t<import>picard_macros.xml</import>\n\t</macros>\n')
+            shell_tmpl['command'] = Template('\t<command detect_errors="exit_code"><![CDATA[\n\t\t@CMD_BEGIN@ $short_name\n\t\t$cheetah_template\n\t\t#include source=$$picard_ref_opts#\n\t\t#include source=$$picard_opts#\n\t\t#include source=$$picard_output_opts#\n\t]]></command>\n')
+            shell_tmpl['inputs'] = Template('\t<inputs>\n\t\t<expand macro="gatk_req_params" />\n\t\t<expand macro="picard_params" />')
+
+        else:
+            shell_tmpl['macros'] = Template('\t<macros>\n\t\t<import>macros.xml</import>\n\t</macros>\n')
+            shell_tmpl['command'] = Template('\t<command detect_errors="exit_code"><![CDATA[\n\t\t@CMD_BEGIN@ $short_name\n\t\t$cheetah_template\n\t]]></command>\n')
+            shell_tmpl['inputs'] = Template('\t<inputs>\n\t\t<expand macro="gatk_req_params" />')
         return shell_tmpl
 
 
@@ -293,7 +368,7 @@ def main():
     :return:
     """
     args = supply_args()
-    myshell = JsonShell(args.json)
+    myshell = JsonShell(args.json, args.picard)
     myshell.get_shell(args.xml_out)
 
 if __name__ == "__main__":
